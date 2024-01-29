da07b4fa4075ddfc3ab88e90cfa32c3698a3f01d
Murazaki <Murazaki@users.noreply.github.com>
2024-01-29 09:23:52 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/assets/README.md.L2hP20oz.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as s,V as a}from"./chunks/framework.wUJtDaaB.js";const k=JSON.parse('{"title":"Pixelfed docs-next","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),i={name:"README.md"},r=a("",6),n=[r];function d(l,o,h,p,c,g){return s(),t("div",null,n)}const u=e(i,[["render",d]]);export{k as __pageData,u as default};
